bf46fb85d92b75fba1e717ed36969a160227242f
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2024-03-25 10:10:28 +0700

    add prosedur input

--- Pertemuanke5/Pertemuanke5.cpp
+++ Pertemuanke5/Pertemuanke5.cpp
@@ -1,2 +1,20 @@
 #include <iostream>
 using namespace std;
+int a, b;
+
+void input()
+{
+    cout << "Bilangan Pertama = ";
+    cin >> a;
+
+    cout << "Bilangan kedua = ";
+    cin >> b;
+}
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
